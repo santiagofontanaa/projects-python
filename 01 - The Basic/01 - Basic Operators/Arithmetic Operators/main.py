7f8f6dbb3df38038b69c107037e07f96d8e24298
Santiago Corsi Fontana <79043997+santiagofontanaa@users.noreply.github.com>
2024-06-02 14:06:47 -0300

    Create main.py

---
+++ 01 - The Basic/01 - Basic Operators/Arithmetic Operators/main.py
@@ -0,0 +1,16 @@
+a = 10
+b = 15
+
+addition_result = a + b # Output: 25
+
+substraction_result = a - b # Output: -5
+
+multiplication_result = a * b # Output: 150
+
+division_result = a / b # Output: 0.666
+
+modulus_result = a % b # Output: 10
+
+exponent_result = a ** b # Output: 100000000
+
+floordivision_result = a // b # Output: 0
