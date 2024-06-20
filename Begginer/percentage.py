aa0333b26a97f70db0ebfba723c990f1cc0781f7
Santiago Corsi Fontana <79043997+santiagofontanaa@users.noreply.github.com>
2024-06-20 14:48:41 -0300

    Update percentage.py

--- Begginer/percentage.py
+++ Begginer/percentage.py
@@ -1,7 +1,7 @@
 # We create the Money Input
-money = input("Mencione el total de la cuenta: ")
+money = input("Enter the amount of money: ")
 # We create the Percentage Input
-percentage = input("Ingresa el total de porcentaje: ")
+percentage = input("Enter percentage amount: ")
 
 # We calculate the percentage in the variable percentage_total
 percentage_total = (int(money) / 100*int(percentage)) 
